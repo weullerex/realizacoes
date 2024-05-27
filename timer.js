5101d48e5d3d726a83cbfc70c42a9b211a02fabb
Weuller Silva <161498130+weullerex@users.noreply.github.com>
2024-05-27 13:46:09 -0300

    Add files via upload
    modelo de cronometro

---
+++ timer.js
@@ -0,0 +1,87 @@
+// function criaHoraDosSegundos(segundos) {
+//     const data = new Date (segundos * 1000);
+//     return data.toLocaleTimeString('pt-BR', {
+//         hour12: false,
+//         timeZone: 'UTC'
+//     });
+// }
+
+// const relogio = document.querySelector('.relogio');
+// const iniciar = document.querySelector('.iniciar');
+// const pausar = document.querySelector('.pausar');
+// const zerar = document.querySelector('.zerar');
+// let segundos = 0;
+// let timer;
+
+// function iniciaRelogio() {
+//      timer = setInterval(function(){
+//         segundos++;
+//         relogio.innerHTML = criaHoraDosSegundos(segundos);
+//     }, 1000);
+// }
+
+// iniciar.addEventListener('click', function(event){
+//     clearInterval(timer);
+//     iniciaRelogio();
+//     relogio.classList.remove('pausado');
+
+// });
+
+// pausar.addEventListener('click', function(event){
+//     clearInterval(timer);
+//     relogio.classList.add('pausado');
+// });
+
+// zerar.addEventListener('click', function(event){
+//     clearInterval(timer);
+//     relogio.innerHTML = '00:00:00';
+//     segundos = 0;
+//     relogio.classList.remove('pausado');
+// });
+
+//     outra forma de fazer
+
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+      const data = new Date(segundos * 1000);
+      return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+      });
+    }
+  
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+  
+    function iniciaRelogio() {
+      timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+      }, 1000);
+    }
+  
+    document.addEventListener('click', function(e) {
+      const el = e.target;
+  
+      if (el.classList.contains('zerar')) {
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado');
+        segundos = 0;
+      }
+  
+      if (el.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+      }
+  
+      if (el.classList.contains('pausar')) {
+        clearInterval(timer);
+        relogio.classList.add('pausado');
+      }
+    });
+  }
+  relogio();
+  
